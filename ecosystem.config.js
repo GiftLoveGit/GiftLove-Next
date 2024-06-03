c4b2398fcaa264348702747dced262f77527bf26
leon paolo <leon.paolo.br.lp@gmail.com>
2024-06-03 17:14:22 -0300

    testing 2 deploy pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       host: ["143.110.230.44", "10.48.0.5", "10.124.0.2"],  // Substitua pelo host SSH
       ref: 'origin/main',  // Branch do Git para fazer o deploy
       repo: 'git@github.com:GiftLoveGit/GiftLove-Next.git',  // Repositório Git
-      path: '/var/www/html/GiftLove-Next',  // Caminho de destino no servidor
+      path: '/var/www/html',  // Caminho de destino no servidor
       'pre-deploy-local': '',  // Comandos antes do deploy local (opcional)
       // 'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',  // Comandos após o deploy
       'pre-setup': ''  // Comandos antes do setup (opcional)
