da43715fffe856ca5cb2a7d1284aed1c1db78444
leon paolo <leon.paolo.br.lp@gmail.com>
2024-06-03 21:51:58 -0300

    url env images

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,23 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ["localhost","homologacao.giftlove.com.br", "homoapi.giftlove.com.br"],
+        // domains: ["localhost","homologacao.giftlove.com.br", "homoapi.giftlove.com.br"],
+        // loader: 'custom',
+        // loaderFile: './src/image/loader.ts',
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'homoapi.giftlove.com.br',
+                port: '',
+                pathname: '/storage/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '',
+                pathname: '/storage/**',
+            },
+        ],
     },
     env: {
         apiStorage: 'https://homoapi.giftlove.com.br/storage',
